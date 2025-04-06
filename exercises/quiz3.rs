9f755e7e6ea5b783778ae96ce4d5ca00020b0edf
白OS <salanhess@126.com>
2025-04-06 22:15:29 +0800

    update

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -13,18 +13,40 @@
 // Make the necessary code changes in the struct ReportCard and the impl block
 // to support alphabetical report cards. Change the Grade in the second test to
 // "A+" to show that your changes allow alphabetical grades.
-//
+//To find the best solution to this challenge you're going to need to think back to your
+//knowledge of traits, specifically Trait Bound Syntax
+// -  you may also need this: `use std::fmt::Display;`.
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+/*
+让我帮你完成这个关于成绩单的练习。这个练习要求我们修改 ReportCard 结构体，使其能够同时支持数字成绩和字母成绩。
+我们需要：
+使用泛型来让 grade 字段支持不同类型
+添加 trait bound 来确保这些类型可以被格式化为字符串
+修改测试用例中的字母成绩
+让我来修改代码：
+做了以下修改：
+添加了 use std::fmt::Display; 来引入 Display trait
+将 ReportCard 改为泛型结构体 ReportCard<T: Display>，其中 T 是成绩的类型
+使用 Display trait bound 确保任何用作成绩的类型都可以被格式化为字符串
+相应地修改了 impl 块，使其支持泛型
+在第二个测试用例中，将数字成绩 2.1 改为字母成绩 "A+"
+这些修改允许 ReportCard 结构体同时支持：
+数字成绩（如第一个测试中的 2.1）
+字母成绩（如第二个测试中的 "A+"）
+现在代码应该可以通过所有测试了。这个解决方案的优点是：
+类型安全：编译器会确保只有实现了 Display trait 的类型才能用作成绩
+*/
 
-pub struct ReportCard {
-    pub grade: f32,
+use std::fmt::Display;
+
+pub struct ReportCard<T: Display> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+impl<T: Display> ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
@@ -50,9 +72,8 @@ mod tests {
 
     #[test]
     fn generate_alphabetic_report_card() {
-        // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+",
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
--- exercises/traits/traits5.rs
+++ exercises/traits/traits5.rs
@@ -7,7 +7,18 @@
 // Execute `rustlings hint traits5` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/*
+这道题目的关键是要实现一个函数 some_func，它接受一个同时实现了 SomeTrait 和 OtherTrait 的参数。
+从代码中我们可以看到，SomeStruct 和 OtherStruct 都实现了这两个 trait。
+我们需要使用 trait bounds 来指定参数类型。
+在这种情况下，我们可以使用 impl trait 语法或者泛型语法。让我修改代码：
+我使用了 impl SomeTrait + OtherTrait 作为参数类型，这表示：
+参数 item 必须实现 SomeTrait trait
+同时也必须实现 OtherTrait trait
++ 符号用于组合多个 trait bounds
+这样修改后，函数就可以接受任何同时实现了这两个 trait 的类型作为参数。在这个例子中，SomeStruct 和 OtherStruct 都满足这个要求，所以 main 函数中的两个调用都是合法的。
+现在代码应该可以编译通过了。你可以运行测试来验证这个解决方案。
+*/
 
 pub trait SomeTrait {
     fn some_function(&self) -> bool {
@@ -30,7 +41,7 @@ impl SomeTrait for OtherStruct {}
 impl OtherTrait for OtherStruct {}
 
 // YOU MAY ONLY CHANGE THE NEXT LINE
-fn some_func(item: ??) -> bool {
+fn some_func(item: impl SomeTrait + OtherTrait) -> bool {
     item.some_function() && item.other_function()
 }
 
