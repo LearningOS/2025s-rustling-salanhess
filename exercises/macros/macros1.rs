4fd7aa29811eeeef57900a7c6e9ff893559fa64a
白OS <salanhess@126.com>
2025-04-08 00:04:28 +0800

    update

--- exercises/macros/macros1.rs
+++ exercises/macros/macros1.rs
@@ -1,9 +1,26 @@
 // macros1.rs
 //
 // Execute `rustlings hint macros1` or use the `hint` watch subcommand for a
-// hint.
+/*
+error[E0423]: expected function, found macro `my_macro`
+  --> exercises/macros/macros1.rs:15:5
+   |
+15 |     my_macro();
+   |     ^^^^^^^^ not a function
+   |
+help: use `!` to invoke the macro
+   |
+15 |     my_macro!();
+   |             +
 
-// I AM NOT DONE
+error: aborting due to 1 previous error; 1 warning emitted
+
+For more information about this error, try `rustc --explain E0423`.
+hint
+When you call a macro, you need to add something special compared to a
+regular function call. If you're stuck, take a look at what's inside
+`my_macro`.
+*/
 
 macro_rules! my_macro {
     () => {
@@ -12,5 +29,5 @@ macro_rules! my_macro {
 }
 
 fn main() {
-    my_macro();
+    my_macro!();
 }
--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -3,10 +3,15 @@
 // Make me compile, without taking the macro out of the module!
 //
 // Execute `rustlings hint macros3` or use the `hint` watch subcommand for a
-// hint.
+/*
+hint
+In order to use a macro outside of its module, you need to do something
+special to the module to lift the macro out into its parent.
 
-// I AM NOT DONE
+The same trick also works on "extern crate" statements for crates that have
+exported macros, if you've seen any of those around.
 
+*/
 mod macros {
     macro_rules! my_macro {
         () => {
